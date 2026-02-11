4fcbefe3538013ee2829ec7a2f4c9eb8cdafcb62
QANSIGLIERE <qansigliere@gmail.com>
2026-02-11 20:57:49 +0300

    JSTS-38 Add a method to remove milliseconds

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var { getDateEpochFormat, convertEpochToISO, getDateInISOFormat } = require('./lib/lib.js');
+var { getDateEpochFormat, convertEpochToISO, getDateInISOFormat, getNumberOfDaysPerMonth } = require('./lib/lib.js');
 
 console.log(getDateEpochFormat());
 console.log(convertEpochToISO(getDateEpochFormat()));
@@ -6,7 +6,9 @@ console.log(getDateInISOFormat());
 console.log(getDateEpochFormat());
 console.log(convertEpochToISO(getDateEpochFormat(), true));
 console.log(getDateInISOFormat(true));
+console.log(getNumberOfDaysPerMonth(new Date()));
 
 module.exports.getDateEpochFormat = getDateEpochFormat;
 module.exports.convertEpochToISO = convertEpochToISO;
 module.exports.getDateInISOFormat = getDateInISOFormat;
+module.exports.getNumberOfDaysPerMonth = getNumberOfDaysPerMonth;
