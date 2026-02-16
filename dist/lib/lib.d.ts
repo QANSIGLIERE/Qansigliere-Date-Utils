f89339c024809a438811f77d4c1be497b90759ce
QANSIGLIERE <qansigliere@gmail.com>
2026-02-16 12:02:37 +0300

    JSTS-39 Add an option to adjust days

--- dist/lib/lib.d.ts
+++ dist/lib/lib.d.ts
@@ -1,11 +1,11 @@
-export function getDateEpochFormat(): number;
+export function getDateEpochFormat(adjustDays: any): Date;
 export function convertEpochToISO(epochFormat: any, skipMilliseconds: any): string;
-export function getDateInISOFormat(skipMilliseconds: any): string;
+export function getDateISOFormat(adjustDays: any, skipMilliseconds: any): string;
 export function getNumberOfDaysPerMonth(date: any): number;
 export function getFirstDayOfWeek(date: any): Date;
 export function convertToSeconds(date: any): number;
 export function convertToMinutes(date: any): number;
 export function convertToHours(date: any): number;
 export function convertToDays(date: any): number;
-export function getDate(): string;
+export function getDate(adjustDays: any): string;
 //# sourceMappingURL=lib.d.ts.map
