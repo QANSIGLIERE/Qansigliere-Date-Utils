af9882a1429ba92ea647afaa6bf2a6633c7cf968
QANSIGLIERE <qansigliere@gmail.com>
2026-02-16 12:07:46 +0300

    2026.02.16.2

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,12 @@
-import { getDateEpochFormat } from "./lib/lib.js";
-import { convertEpochToISO } from "./lib/lib.js";
-import { getDateISOFormat } from "./lib/lib.js";
-import { getNumberOfDaysPerMonth } from "./lib/lib.js";
-import { getFirstDayOfWeek } from "./lib/lib.js";
-import { convertToSeconds } from "./lib/lib.js";
-import { convertToMinutes } from "./lib/lib.js";
-import { convertToHours } from "./lib/lib.js";
-import { convertToDays } from "./lib/lib.js";
-import { getDate } from "./lib/lib.js";
+import { getDateEpochFormat } from "./dist/lib/lib.js";
+import { convertEpochToISO } from "./dist/lib/lib.js";
+import { getDateISOFormat } from "./dist/lib/lib.js";
+import { getNumberOfDaysPerMonth } from "./dist/lib/lib.js";
+import { getFirstDayOfWeek } from "./dist/lib/lib.js";
+import { convertToSeconds } from "./dist/lib/lib.js";
+import { convertToMinutes } from "./dist/lib/lib.js";
+import { convertToHours } from "./dist/lib/lib.js";
+import { convertToDays } from "./dist/lib/lib.js";
+import { getDate } from "./dist/lib/lib.js";
 export { getDateEpochFormat, convertEpochToISO, getDateISOFormat, getNumberOfDaysPerMonth, getFirstDayOfWeek, convertToSeconds, convertToMinutes, convertToHours, convertToDays, getDate };
 //# sourceMappingURL=index.d.ts.map
